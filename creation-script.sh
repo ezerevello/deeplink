bdb221caf3b28f8cf8d1e3ce0c3126e0db97bc0e
ezerevello <revellojonathan@gmail.com>
2026-06-03 22:42:53 -0300

    updating for 3 nodes

--- creation-script.sh
+++ creation-script.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
 create_netns() {
-# This is for create and setup the Network Namespaces
+# This is for creating and setting up the Network Namespaces
 
     set -e
 
-    # 1) Create eath_base and orb_sat Namespaces
-    ip netns add earth_base
-    ip netns add orb_sat
+    # 1) Create earth-base, orb-sat and m-rover Namespaces
+    ip netns add earth-base
+    ip netns add orb-sat
+    ip netns add m-rover
 
     # 2) Turn on the loopback interface
-    ip netns exec earth_base ip link set lo up
-    ip netns exec orb_sat ip link set lo up
+    ip netns exec earth-base ip link set lo up
+    ip netns exec orb-sat ip link set lo up
+    ip netns exec m-rover ip link set lo up
 
-    # 3) Create and connect Virtual Ethernet
-    ip link add veth_earth type veth peer name veth_sat
-    ip link set veth_earth netns earth_base
-    ip link set veth_sat netns orb_sat
+    # 3) Create and connect Virtual Ethernet between earth-base and orb-sat
+    ip link add veth-earth type veth peer name veth-sat-earth
+    ip link set veth-earth netns earth-base
+    ip link set veth-sat-earth netns orb-sat
 
-    # 3.1) Turn on
-    ip netns exec earth_base ip link set veth_earth up
-    ip netns exec orb_sat ip link set veth_sat up
+    # 4) Between orb-sat and m-rover
+    ip link add veth-sat-rover type veth peer name veth-rover
+    ip link set veth-sat-rover netns orb-sat
+    ip link set veth-rover netns m-rover
 
-    # 4) Assign IP Addresses
-    ip netns exec earth_base ip addr add 10.0.1.1/24 dev veth_earth
-    ip netns exec orb_sat ip addr add 10.0.1.2/24 dev veth_sat
+    # 5) Turn on veth
+    ip netns exec earth-base ip link set veth-earth up
+    ip netns exec orb-sat ip link set veth-sat up
+    ip netns exec m-rover ip link set veth-rover up
+
+    # 6) Assign IP Addresses
+    ip netns exec earth-base ip addr add 10.0.1.1/24 dev veth-earth
+    ip netns exec orb-sat ip addr add 10.0.1.2/24 dev veth-sat
+    ip netns exec m-rover ip addr add 10.0.1.3/24 dev veth-rover
 
     set +e
 }
